1eed606915c9d371b0c50f39fc7d9364e382cde2
Matteo Massaia <MATTEO.MASSAIA@local.ittcesena.it>
2022-11-24 08:36:28 +0100

    step 2:aggiunta la classe Biblioteca

---
+++ verificaGIT/verificaGIT/Biblioteca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificaGIT
+{
+    internal class Biblioteca
+    {
+        public string nomeBiblioteca { get; set; }
+        public string indirizzo { get; set; }
+        public string orarioApertura { get; set; }
+        public string orarioChiusura { get; set; }
+        public List<Libro> elencoLibri { get; set; }
+
+        public Biblioteca(string nomeBiblioteca, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            this.nomeBiblioteca = nomeBiblioteca;
+            this.indirizzo = indirizzo;
+            this.orarioApertura = orarioApertura;
+            this.orarioChiusura = orarioChiusura;
+        }
+    }
+
+    
+}
